16ae9bf747dc224d0442e94d6e79ca4e4972bb1f
ricardo <fkondem@gmail.com>
2024-11-20 20:27:42 +0100

    added blank page fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'; // Import your App component
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import BrowserRouter and Routes
+
 import Register from './pages/Register/register';
-import './App.css';
 import Login from './pages/login/Login';
 import ProtectedRoute from './components/ProtectedRoute';
 import Dashboard from './pages/dashboard/dashboard';
 
-const root = document.getElementById('root') as HTMLElement
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <>welcome</>,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/account",
-    element: <ProtectedRoute> <Dashboard/> </ProtectedRoute>,
-  },
-]);
+const root = document.getElementById('root') as HTMLElement;
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        {/* Main Route */}
+        <Route path="/" element={<App />} />
+
+        {/* Register Route */}
+        <Route path="/register" element={<Register />} />
+
+        {/* Login Route */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Protected Route */}
+        <Route path="/account" element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
